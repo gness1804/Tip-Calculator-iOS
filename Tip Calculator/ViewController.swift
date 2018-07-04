48a8a7bcee50cec976ca1815d99adb48b6c69fd5
gness1804 <graham.nessler@gmail.com>
2018-07-04 13:38:48 -0500

    Add tip amount showing from slider

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var tipPercentOutput: UILabel!
+    
+    @IBAction func onSliderChanged(_ sender: Any) {
+        let val = slider.value
+        tipPercentOutput.text = "Tip Amount: \(val)%"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
