cd699771f23ea6cdf0932b908a21cee1ea3f6121
gness1804 <graham.nessler@gmail.com>
2018-07-05 21:06:02 -0500

    Set default tip percent at 20

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -66,7 +66,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showTipPercentOutput(val: slider.value)
     }
 
     override func didReceiveMemoryWarning() {
